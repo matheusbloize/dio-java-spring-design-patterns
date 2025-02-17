5e73655eb28d5eca26ec441a3127f9a4d14eda51
Matheus Bloize <mbloizepd@gmail.com>
2025-02-17 11:30:52 -0300

    feat: create user model

---
+++ src/main/java/com/matheusbloize/dio_java_spring_design_patterns/models/User.java
@@ -0,0 +1,41 @@
+package com.matheusbloize.dio_java_spring_design_patterns.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_USER")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
