40bcaaa4e7c2aac379f37d81bf4ee1b021b711a3
Matheus Bloize <mbloizepd@gmail.com>
2025-02-17 13:32:38 -0300

    feat: create product model

---
+++ src/main/java/com/matheusbloize/dio_java_spring_design_patterns/models/Product.java
@@ -0,0 +1,76 @@
+package com.matheusbloize.dio_java_spring_design_patterns.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUCT")
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    private LocalDateTime offerExpirationDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getOfferExpirationDate() {
+        return offerExpirationDate;
+    }
+
+    public void setOfferExpirationDate(LocalDateTime offerExpirationDate) {
+        this.offerExpirationDate = offerExpirationDate;
+    }
+
+}
