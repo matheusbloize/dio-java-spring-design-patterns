63f26c73d4240a123c5cf65bc900e79e30889d19
Matheus Bloize <mbloizepd@gmail.com>
2025-02-17 11:13:22 -0300

    feat: start project

---
+++ src/main/java/com/matheusbloize/dio_java_spring_design_patterns/DioJavaSpringDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.matheusbloize.dio_java_spring_design_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioJavaSpringDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioJavaSpringDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusbloize/dio_java_spring_design_patterns/DioJavaSpringDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusbloize.dio_java_spring_design_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioJavaSpringDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
