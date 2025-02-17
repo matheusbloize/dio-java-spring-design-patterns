b36b80750e8ea1f9fdd6bf2ebd437debade9b7fb
Matheus Bloize <mbloizepd@gmail.com>
2025-02-17 15:25:10 -0300

    feat: create product service strategy interface and implementation

---
+++ src/main/java/com/matheusbloize/dio_java_spring_design_patterns/services/ProductService.java
@@ -0,0 +1,23 @@
+package com.matheusbloize.dio_java_spring_design_patterns.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import com.matheusbloize.dio_java_spring_design_patterns.models.Product;
+
+// Strategy Pattern
+public interface ProductService {
+    List<Product> listAll();
+
+    Optional<Product> findById(UUID id);
+
+    Set<Product> findByUser(UUID id);
+
+    Product getOne(UUID id);
+
+    Product save(Product product);
+
+    void delete(Product product);
+}
---
+++ src/main/java/com/matheusbloize/dio_java_spring_design_patterns/services/impl/ProductServiceImpl.java
@@ -0,0 +1,59 @@
+package com.matheusbloize.dio_java_spring_design_patterns.services.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.matheusbloize.dio_java_spring_design_patterns.models.Product;
+import com.matheusbloize.dio_java_spring_design_patterns.repositories.ProductRepository;
+import com.matheusbloize.dio_java_spring_design_patterns.services.ProductService;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    // Singleton Pattern
+    private ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    // Strategy and Facade Patterns
+
+    @Override
+    public List<Product> listAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> findById(UUID id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public Set<Product> findByUser(UUID id) {
+        return productRepository.findById(id).get().getUser().getProducts();
+    }
+
+    @Override
+    public Product getOne(UUID id) {
+        Product product = productRepository.findById(id).get();
+        return product;
+    }
+
+    @Override
+    @Transactional
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Product product) {
+        productRepository.delete(product);
+    }
+}
