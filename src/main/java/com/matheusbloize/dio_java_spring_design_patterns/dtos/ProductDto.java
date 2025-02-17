0534b1256a8ceaf3d9c40e9bd17c8f3776d7ee24
Matheus Bloize <mbloizepd@gmail.com>
2025-02-17 16:45:02 -0300

    fix: change product dto constraints and add user id

--- src/main/java/com/matheusbloize/dio_java_spring_design_patterns/dtos/ProductDto.java
+++ src/main/java/com/matheusbloize/dio_java_spring_design_patterns/dtos/ProductDto.java
@@ -2,12 +2,15 @@ package com.matheusbloize.dio_java_spring_design_patterns.dtos;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record ProductDto(@NotBlank String name, @NotBlank @Min(value = 1) BigDecimal price, @NotBlank @JsonFormat(pattern = "dd/MM/yyyy HH:mm") LocalDateTime expirationDate) {
+public record ProductDto(@NotBlank String name, @NotNull @Min(value = 1) BigDecimal price, @NotNull @JsonFormat(pattern = "dd/MM/yyyy HH:mm") LocalDateTime expirationDate,
+                @NotNull UUID userId) {
 
 }
