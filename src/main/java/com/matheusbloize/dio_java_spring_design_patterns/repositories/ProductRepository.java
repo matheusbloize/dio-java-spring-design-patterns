3083cc4922a15614aa70d8a14b08f9b46aee42b9
Matheus Bloize <mbloizepd@gmail.com>
2025-02-18 15:25:01 -0300

    feat: add specification on repository

--- src/main/java/com/matheusbloize/dio_java_spring_design_patterns/repositories/ProductRepository.java
+++ src/main/java/com/matheusbloize/dio_java_spring_design_patterns/repositories/ProductRepository.java
@@ -3,11 +3,11 @@ package com.matheusbloize.dio_java_spring_design_patterns.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.matheusbloize.dio_java_spring_design_patterns.models.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID> {
-
+public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
 }
