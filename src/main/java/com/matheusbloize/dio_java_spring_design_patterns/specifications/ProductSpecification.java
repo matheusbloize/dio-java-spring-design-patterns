85bb13e7f958d5eae75b957202e4528df1fead36
Matheus Bloize <mbloizepd@gmail.com>
2025-02-18 15:26:00 -0300

    feat: create product specification with sort by creatin date spec

---
+++ src/main/java/com/matheusbloize/dio_java_spring_design_patterns/specifications/ProductSpecification.java
@@ -0,0 +1,17 @@
+package com.matheusbloize.dio_java_spring_design_patterns.specifications;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.matheusbloize.dio_java_spring_design_patterns.models.Product;
+
+public class ProductSpecification {
+    public static Specification<Product> sortByCreationDate(boolean sortAsc) {
+        return (root, query, builder) -> {
+            if (sortAsc) {
+                return builder.conjunction();
+            }
+            query.orderBy(builder.desc(root.get("creationDate")));
+            return builder.conjunction();
+        };
+    }
+}
