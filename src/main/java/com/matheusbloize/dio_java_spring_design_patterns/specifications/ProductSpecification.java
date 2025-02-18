f91dd1949f59be55abd15ea6773f30229e12a59a
Matheus Bloize <mbloizepd@gmail.com>
2025-02-18 16:00:14 -0300

    refactor: add condition to stop future bugs

--- src/main/java/com/matheusbloize/dio_java_spring_design_patterns/specifications/ProductSpecification.java
+++ src/main/java/com/matheusbloize/dio_java_spring_design_patterns/specifications/ProductSpecification.java
@@ -1,12 +1,16 @@
 package com.matheusbloize.dio_java_spring_design_patterns.specifications;
 
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.ObjectUtils;
 
 import com.matheusbloize.dio_java_spring_design_patterns.models.Product;
 
 public class ProductSpecification {
     public static Specification<Product> sortByCreationDate(String sort) {
         return (root, query, builder) -> {
+            if (ObjectUtils.isEmpty(sort)) {
+                return null;
+            }
             if (sort.equalsIgnoreCase("asc")) {
                 return builder.conjunction();
             }
